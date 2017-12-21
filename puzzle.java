5068989a617a54c408bbbbb6489c901f4b82ccf3
rafaiy <rafaiy.rehman@gmail.com>
2017-12-21 14:04:48 +0530

    Picture Puzzle
    Puzzle

---
+++ puzzle.java
@@ -0,0 +1,16 @@
+
+public class puzzle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		game pz=new game();
+		pz.setDefaultCloseOperation(pz.EXIT_ON_CLOSE);
+		pz.setSize(808,612);
+		pz.setVisible(true);
+
+	}
+
+}
